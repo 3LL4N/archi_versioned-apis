9c32fe15e3c8dacced5b1e45774ca90d60b823c8
unknown <dierongamer04@gmail.com>
2022-04-17 17:58:15 +0800

    Fixed ts syntax for backend request-response

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,25 @@
 import cors from 'cors';
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
-const express = require('express')
+const express = require('express');
 
 const app = express();
 
 const db = new PrismaClient();
 
 app
-    .use(cors())
-    .use(bodyParser.json())
-    .get('/people', (request, response) => {
-        
-    })
-    .listen(8000, () => {
-        console.log('Server started at http://localhost:8000')
-    })
-
+  .use(cors())
+  .use(bodyParser.json())
+  .get('/people', (request: any, response: any) => {
+    db
+      .person
+      .findMany()
+      .then((people) => {
+        response.json(people);
+      });
+  })
+  .listen(8000, () => {
+    console.log('Server started at http://localhost:8000');
+  });
