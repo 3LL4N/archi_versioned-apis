229625202c8d2a4fa09d3c7b9c021b2d16627019
unknown <dierongamer04@gmail.com>
2022-04-17 17:34:24 +0800

    Linked db to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,21 @@
-import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser'
+import {PrismaClient} from '@prisma/client'
 
+const bodyParser = require('body-parser')
+
+const express = require('express')
 
 const app = express();
 
+const db = new PrismaClient();
 
+app
+    .use(cors())
+    .use(bodyParser.json())
+    .get('/people', (request, response) => {
+        
+    })
+    .listen(8000, () => {
+        console.log('Server started at http://localhost:8000')
+    })
 
