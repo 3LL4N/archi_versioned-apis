5dce1d4f99e3a84931f4ee715226382809980771
unknown <dierongamer04@gmail.com>
2022-04-17 19:39:37 +0800

    Added interface for person v1 and v2

--- api_ver2/backend/src/index.ts
+++ api_ver2/backend/src/index.ts
@@ -11,6 +11,18 @@ const db = new PrismaClient();
 
 const PORT = 5050;
 
+interface PersonV1 {
+  id: number;
+  firstName: string;
+  lastName: string;
+}
+
+interface PersonV2 {
+  id: number;
+  familyName: string;
+  givenName: string;
+}
+
 app
   .use(cors())
   .use(bodyParser.json())
@@ -21,6 +33,13 @@ app
       .then((people) => {
         response.json(people);
       });
+  })
+  .get('/v2/people', (request: any, response: any) => {
+    db
+      .person
+      .findMany()
+      .then((people) => {
+        response.json(people);
   })
   .listen(PORT, () => {
     console.log('Server started at localhost:5050');
