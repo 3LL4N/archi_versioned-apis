d07ecb6345222cd26ec7305733d67b67405a0305
unknown <dierongamer04@gmail.com>
2022-04-17 18:18:10 +0800

    Added un react-native-dotenv

--- nativeProj/App.tsx
+++ nativeProj/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
